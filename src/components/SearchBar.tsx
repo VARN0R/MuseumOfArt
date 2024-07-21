4086eaa08e5c4e30ff8c7d9a460243aa03c482e1
Vladislav <vladislav.malchevskiy.work@gmail.com>
2024-07-21 17:36:34 +0300

    add search bar with debounce

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import styled from 'styled-components';
+import { debounce } from 'lodash';
+import Container from './Container';
+
+const ButtonStyled = styled.button`
+  all: unset;
+  cursor: pointer;
+  position: absolute;
+  top: 16px;
+  right: 16px;
+`;
+
+const FormStyled = styled(Form)`
+  position: relative;
+  width: 762px;
+  margin: 72px auto 0 auto;
+`;
+
+const FieldStyled = styled(Field)`
+  border-radius: 16px;
+  padding: 16px;
+  width: 762px;
+  height: 64px;
+  background: rgba(57, 57, 57, 0.05);
+  border: none;
+`;
+
+const ErrorMessageStyled = styled(ErrorMessage)`
+  color: red;
+`;
+
+const SearchBar: React.FC<{ onSubmit: (values: any) => void }> = ({
+  onSubmit,
+}) => {
+  const validationSchema = Yup.object({
+    query: Yup.string().required('Search query is required'),
+  });
+
+  const debouncedSubmit = debounce(onSubmit, 3000);
+
+  return (
+    <Container>
+      <Formik
+        initialValues={{ query: '' }}
+        validationSchema={validationSchema}
+        onSubmit={debouncedSubmit}
+      >
+        <FormStyled>
+          <FieldStyled
+            name="query"
+            type="text"
+            placeholder="Search Art, Artist, Work..."
+          />
+          <ErrorMessageStyled name="query" component="div" />
+          <ButtonStyled type="submit">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.5 25C20.299 25 25 20.299 25 14.5C25 8.70101 20.299 4 14.5 4C8.70101 4 4 8.70101 4 14.5C4 20.299 8.70101 25 14.5 25Z"
+                stroke="#393939"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M28 28L22 22"
+                stroke="#393939"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </ButtonStyled>
+        </FormStyled>
+      </Formik>
+    </Container>
+  );
+};
+
+export default SearchBar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
 import Title from '../components/Title';
+import axios from 'axios';
 
 const Home = () => {
+  const [artworks, setArtworks] = useState([]);
+  console.log(artworks);
+
+  const handleSearch = async (values: any) => {
+    const response = await axios.get(
+      `https://api.artic.edu/api/v1/artworks/search?q=${values.query}`
+    );
+
+    setArtworks(response.data.data);
+  };
+
   return (
     <div>
       <Header></Header>
       <Title></Title>
+      <SearchBar onSubmit={handleSearch}></SearchBar>
     </div>
   );
 };
