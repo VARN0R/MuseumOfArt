227f90847608dd1142318c8e6d4c5103797419a8
Vladislav <vladislav.malchevskiy.work@gmail.com>
2024-07-23 14:50:52 +0300

    replace svg code on the svg images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import Container from './Container';
 import logoHeader from '../assets/img/logoHeader.svg';
+import homeIcon from '../assets/img/homeIcon.svg';
+import favoritesIcon from '../assets/img/favoritesIcon.svg';
 
 const HeaderStyled = styled.header`
   background: linear-gradient(
@@ -84,49 +86,14 @@ const Header = () => {
           <Links>
             <NavItem display={navItemHomeDisplay}>
               <Icon>
-                <svg
-                  width="24"
-                  height="25"
-                  viewBox="0 0 24 25"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M3 9.5L12 2.5L21 9.5V20.5C21 21.0304 20.7893 21.5391 20.4142 21.9142C20.0391 22.2893 19.5304 22.5 19 22.5H5C4.46957 22.5 3.96086 22.2893 3.58579 21.9142C3.21071 21.5391 3 21.0304 3 20.5V9.5Z"
-                    stroke="#E0A449"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M9 22.5V12.5H15V22.5"
-                    stroke="#E0A449"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                <img src={homeIcon} alt="home icon" />
               </Icon>
               <LinkStyled to="/">Home</LinkStyled>
             </NavItem>
 
             <NavItem display={navItemHomeDisplay}>
               <Icon>
-                <svg
-                  width="24"
-                  height="25"
-                  viewBox="0 0 24 25"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M19.5 21.5L12.375 17.5L5.25 21.5V5.5C5.25 4.96957 5.46448 4.46086 5.84625 4.08579C6.22802 3.71071 6.74581 3.5 7.28571 3.5H17.4643C18.0042 3.5 18.522 3.71071 18.9038 4.08579C19.2855 4.46086 19.5 4.96957 19.5 5.5V21.5Z"
-                    stroke="#E0A449"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                <img src={favoritesIcon} alt="favorites icon" />
               </Icon>
               <LinkStyled to="/favorites">Your favorites</LinkStyled>
             </NavItem>
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Art from '../types/Art';
 import Card from '../components/Card';
+import favoritesTitle from '../assets/img/favoritesTitle.svg';
 
 const Title = styled.h2`
   font-weight: 700;
@@ -18,6 +19,7 @@ const Title = styled.h2`
   width: 690px;
   margin: 120px auto 0 auto;
   span {
+    display: block;
     color: #f17900;
   }
 `;
@@ -27,6 +29,15 @@ const FavoritesContainer = styled.div`
   grid-gap: 10px;
   grid-template-columns: 1fr 1fr 1fr;
   margin-top: 40px;
+`;
+
+const FavoritesIconTitle = styled.img`
+  display: inline-block;
+`;
+
+const FavoritesTitleUnder = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 interface FavoriteProps {
@@ -57,26 +68,13 @@ const Favorites: React.FC<FavoriteProps> = ({ arts }) => {
       <Container>
         <Title>
           Here are your{' '}
-          <div>
-            {
-              <svg
-                width="74"
-                height="74"
-                viewBox="0 0 74 74"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M53.2573 57.9032L36.9993 48.4409L20.7412 57.9032V20.0538C20.7412 18.799 21.2306 17.5956 22.1017 16.7083C22.9729 15.821 24.1544 15.3226 25.3864 15.3226H48.6122C49.8442 15.3226 51.0257 15.821 51.8968 16.7083C52.7679 17.5956 53.2573 18.799 53.2573 20.0538V57.9032Z"
-                  stroke="#F17900"
-                  stroke-width="4.69765"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            }
+          <FavoritesTitleUnder>
+            <FavoritesIconTitle
+              src={favoritesTitle}
+              alt="favorites icon title"
+            ></FavoritesIconTitle>
             <span>Favorites</span>
-          </div>
+          </FavoritesTitleUnder>
         </Title>
       </Container>
       <Subtitle
