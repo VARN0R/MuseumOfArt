ed69b7240e85ca6542fbd4227e25d9cc0e8808b5
Vladislav <vladislav.malchevskiy.work@gmail.com>
2024-07-23 17:46:20 +0300

    add error boundary

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,39 +6,64 @@ import Details from '../pages/Details';
 import Favorites from '../pages/Favorites';
 import axios from 'axios';
 import Art from '../types/Art';
+import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
+
+interface ErrorFallbackProps {
+  error: Error;
+  resetErrorBoundary: () => void;
+}
+
+const ErrorFallback: React.FC<ErrorFallbackProps> = ({
+  error,
+  resetErrorBoundary,
+}) => {
+  return (
+    <div role="alert">
+      <p>Ой</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Попробовать снова</button>
+    </div>
+  );
+};
 
 function App() {
   const [arts, setArts] = useState<Art[]>([]);
   useEffect(() => {
     const fetchArtworks = async () => {
-      axios
-        .get('https://api.artic.edu/api/v1/artworks', {
-          params: {
-            page: 1,
-            limit: 50,
-          },
-        })
-        .then((response) => {
-          const artworks = response.data.data.map((art: any) => ({
-            id: art.id,
-            title: art.title,
-            artist: art.artist_title,
-            imageUrl: `https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`,
-          }));
-          setArts(artworks);
-        });
+      try {
+        axios
+          .get('https://api.artic.edu/api/v1/artworks', {
+            params: {
+              page: 1,
+              limit: 50,
+            },
+          })
+          .then((response) => {
+            const artworks = response.data.data.map((art: any) => ({
+              id: art.id,
+              title: art.title,
+              artist: art.artist_title,
+              imageUrl: `https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`,
+            }));
+            setArts(artworks);
+          });
+      } catch (error) {
+        console.error('Error fetching artworks', error);
+      }
     };
 
     fetchArtworks();
   }, []);
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home arts={arts} />} />
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/favorites" element={<Favorites arts={arts} />} />
-      </Routes>
-    </Router>
+    <ReactErrorBoundary FallbackComponent={ErrorFallback}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home arts={arts} />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/favorites" element={<Favorites arts={arts} />} />
+        </Routes>
+      </Router>
+    </ReactErrorBoundary>
   );
 }
 
