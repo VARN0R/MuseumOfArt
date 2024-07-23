7e087e2a23db71a186eb1b6df98eb830b9c7175e
Vladislav <vladislav.malchevskiy.work@gmail.com>
2024-07-23 12:37:12 +0300

    add filter in the gallery component

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -12,6 +12,18 @@ const GalleryContainer = styled.div`
   margin-top: 40px;
 `;
 
+const SortSelect = styled.select`
+  padding: 10px 20px;
+  outline: none;
+  cursor: pointer;
+  border: 1px solid #f0f1f1;
+`;
+
+const SortContainer = styled.div`
+  width: 150px;
+  margin: 40px auto 0 auto;
+`;
+
 interface GalleryProps {
   arts: Art[];
 }
@@ -21,6 +33,11 @@ const Gallery: React.FC<GalleryProps> = ({ arts }) => {
     const storedFavorites = localStorage.getItem('favorites');
     return storedFavorites ? JSON.parse(storedFavorites) : [];
   });
+
+  const [sortType, setSortType] = useState<string>('none');
+
+  const [isSortedAlphabetically, setIsSortedAlphabetically] =
+    useState<boolean>(false);
 
   useEffect(() => {
     localStorage.setItem('favorites', JSON.stringify(favorites));
@@ -32,7 +49,23 @@ const Gallery: React.FC<GalleryProps> = ({ arts }) => {
     );
   };
 
-  const galleryArts = arts.slice(13, 22);
+  let galleryArts = arts.slice(13, 22);
+
+  const handleSortChange = (event: any) => {
+    setSortType(event.target.value);
+  };
+
+  if (sortType === 'title') {
+    galleryArts = [...galleryArts].sort((a, b) =>
+      a.title.localeCompare(b.title)
+    );
+  } else if (sortType === 'artist') {
+    galleryArts = [...galleryArts].sort((a, b) => {
+      const artistA = a.artist || '';
+      const artistB = b.artist || '';
+      return artistA.localeCompare(artistB);
+    });
+  }
 
   return (
     <div>
@@ -41,6 +74,13 @@ const Gallery: React.FC<GalleryProps> = ({ arts }) => {
         topText="Here some more"
       ></Subtitle>
       <Container>
+        <SortContainer>
+          <SortSelect value={sortType} onChange={handleSortChange}>
+            <option value="none">No Sort</option>
+            <option value="title">Sort by Title</option>
+            <option value="artist">Sort by Artist</option>
+          </SortSelect>
+        </SortContainer>
         <GalleryContainer>
           {galleryArts.map((art) => (
             <Card
